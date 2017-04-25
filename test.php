bc1c9a8e3de90bb1b026c7b3ff2799d038d54755
Grant Recker <gwcrecker@gmail.com>
2017-04-25 13:33:28 -0400

    test

--- test.php
+++ test.php
@@ -1,31 +1,7 @@
 <?php
-if<!isset($_POST['submit']
-          {
-            //This page should not be accessed directly. Need to submit the form.
-            echo "error; you need to submit the form!";
-          }
-          $name = $_POST['name'];
-          $email = $_POST['email'];
-          $message = $_POST['message'];
 
-//validate first
-if(empty($name) || empty($email))
-          {
-            echo "Name and email are manditory!";
-            exit;
-          }
-$email_from='reckeraerial@gmail.com';
-$email_subject="New Form Submission";
-$email_body="You have recieved a new message from the user $name. \n".
-    "email address: $visitor_email\n".
-    "here is the message: \n $message".
-  
-$to= "reckeraerial@gmail.com";
-$headers= "From: $email_from \r\n";
+$message = "test code to see if this works";
 
-          
-          //send the email
-    
-    mail($to,$email_subject,$email,$email_body,$headers);
-          //sent
-          
+mail('reckeraerial@gmail.com', 'Subject', $message);
+
+?>
