5d2f6dedbeeac80b15e12f1030807c043d88ceff
peter-goebel <pgoebel@sewickley.org>
2017-04-25 13:22:25 -0400

    part 2 of test php

--- test.php
+++ test.php
@@ -14,4 +14,18 @@ if(empty($name) || empty($email))
             echo "Name and email are manditory!";
             exit;
           }
+$email_from=@reckeraerial@gmail.com;
+$email_subject="New Form Submission";
+$email_body="YOu have recieved a new message from the user $name. \n".
+    "email address: $visitor_email\n".
+    "here is the message: \n $message".
+  
+$to=reckeraerial@gmail.com 
+$headers="From: $email_from \r\n";
+
+          
+          //send the email
+    
+    mail($to,$email_subject,$email,$email_body,$headers);
+          //sent
           
