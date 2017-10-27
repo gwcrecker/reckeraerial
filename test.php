2066fc84d1943a97979d26bc392ff6d082f2d8d1
peter-goebel <pgoebel@sewickley.org>
2017-10-27 09:19:34 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,14 +1,15 @@
 <?php
 
+
 if(!isset($_POST['submit']))
           {
             //This page should not be accessed directly. Need to submit the form.
             echo "error; you need to submit the form!";
           }
  
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+$name = $_POST[name];
+$email = $_POST[email];
+$message = $_POST[message];
 
 //validate first
 if(empty($name) || empty($email))
